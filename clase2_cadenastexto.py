e213e7ae074a37aca38bc317d29335bd4a811f7e
Diego Avellaneda <daacdiego2015@gmail.com>
2025-03-04 13:38:45 -0500

    Clase 2 cadenas de texto

---
+++ clase2_cadenastexto.py
@@ -0,0 +1,17 @@
+#Formas para representar cadenas
+
+#1 
+name="Diego"  #comillas dobles
+lastname='Avellaneda' #comillas simples
+fullname='''  
+Diego
+Alexander
+Avellaneda
+'''   #Comillas triples, respetan los saltos de linea 
+
+print(name)
+print(lastname)
+print(fullname)
+
+# Manipulación de cadenas de texto
+
